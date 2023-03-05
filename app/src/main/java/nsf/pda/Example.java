43702d1413c4ce82c392880e542d68186da41af2
rtatton <rdt17@proton.me>
2023-03-05 18:49:33 -0500

    feat: update example to use AuthInterceptor

--- app/src/main/java/nsf/pda/Example.java
+++ app/src/main/java/nsf/pda/Example.java
@@ -1,7 +1,10 @@
 package nsf.pda;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
+import nsf.pda.auth.AuthInterceptor;
 import nsf.pda.auth.Authenticator;
+import nsf.pda.auth.BasicCredentials;
+import nsf.pda.auth.TokenService;
 import nsf.pda.data.DataService;
 import okhttp3.OkHttpClient;
 import retrofit2.Retrofit;
@@ -14,15 +17,20 @@ public class Example {
     OkHttpClient client = new OkHttpClient.Builder()
         .build();
     Retrofit retrofit = new Retrofit.Builder()
-        .baseUrl("https://example.hubat.net/")
+        .baseUrl("https://username.hubat.net/")
         .addConverterFactory(JacksonConverterFactory.create(mapper))
         .client(client)
         .build();
-    Authenticator authService = retrofit.create(Authenticator.class);
+    Authenticator authenticator = retrofit.create(Authenticator.class);
+    TokenService tokenService = TokenService.builder()
+        .credentials(BasicCredentials.of("username", "password"))
+        .authenticator(authenticator)
+        .build();
     OkHttpClient authenticatedClient = client.newBuilder()
+        .addInterceptor(AuthInterceptor.of(tokenService))
         .build();
     Retrofit authenticatedRetrofit = retrofit.newBuilder()
-        .baseUrl("https://example.hubat.net/api/v2.6/")
+        .baseUrl("https://username.hubat.net/api/v2.6/")
         .client(authenticatedClient)
         .build();
     DataService dataService = authenticatedRetrofit.create(DataService.class);
