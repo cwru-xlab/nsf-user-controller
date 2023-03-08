f0bb2ab628e9bd541105b568f813f337bb2d3e02
rtatton <rdt17@proton.me>
2023-03-07 22:34:15 -0500

    fix: enable endpoint encoding for proper URL formatting

--- app/src/main/java/nsf/pda/data/DataService.java
+++ app/src/main/java/nsf/pda/data/DataService.java
@@ -13,16 +13,19 @@ import retrofit2.http.Query;
 public interface DataService {
 
   @GET("data/{endpoint}")
-  Call<List<Record>> get(@Path("endpoint") String endpoint, @Body GetOptions options);
+  Call<List<Record>> get(
+      @Path(value = "endpoint", encoded = true) String endpoint, @Body GetOptions options);
 
   @GET("data/{endpoint}")
-  Call<List<Record>> get(@Path("endpoint") String endpoint);
+  Call<List<Record>> get(@Path(value = "endpoint", encoded = true) String endpoint);
 
   @POST("data/{endpoint}")
-  Call<List<Record>> post(@Path("endpoint") String endpoint, @Body List<Record> records);
+  Call<List<Record>> post(
+      @Path(value = "endpoint", encoded = true) String endpoint, @Body List<Record> data);
 
   @POST("data/{endpoint}")
-  Call<Record> post(@Path("endpoint") String endpoint, @Body Record record);
+  Call<Record> post(
+      @Path(value = "endpoint", encoded = true) String endpoint, @Body Record data);
 
   @PUT("data")
   Call<List<Record>> put(@Body List<Record> records);
@@ -30,4 +33,6 @@ public interface DataService {
   @DELETE("data")
   Call<Void> delete(@Query("records") String... recordIds);
 
+  @DELETE("data")
+  Call<Void> delete(@Query("records") List<String> recordIds);
 }
