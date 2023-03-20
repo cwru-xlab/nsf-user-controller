dc49b0a05ff825e0a0808105238c693790a725cc
rtatton <rdt17@proton.me>
2023-03-20 10:34:22 -0500

    refactor: move compile-time validation into run-time getters

--- app/src/main/java/nsf/vertx/BaseToken.java
+++ app/src/main/java/nsf/vertx/BaseToken.java
@@ -1,53 +1,31 @@
 package nsf.vertx;
 
 import static com.google.common.base.Preconditions.checkNotNull;
-import static com.google.common.base.Preconditions.checkState;
 
 import io.jsonwebtoken.Claims;
-import io.jsonwebtoken.Jwt;
+import io.jsonwebtoken.Jws;
 import java.time.Duration;
 import java.time.Instant;
-import java.util.Date;
 import org.immutables.value.Value;
 
 @Value.Immutable
 abstract class BaseToken {
 
-  private static final String IAT_PATH = "decoded.body.iat";
-  private static final String EXP_PATH = "decoded.body.exp";
-
   @Value.Parameter(order = 1)
   public abstract String encoded();
 
   @Value.Parameter(order = 2)
-  public abstract Jwt<?, Claims> decoded();
+  public abstract Jws<Claims> decoded();
 
   public Duration ttl() {
-    Instant iat = iat().toInstant();
-    Instant exp = exp().toInstant();
-    return Duration.between(iat, exp);
+    return Duration.between(iat(), exp());
   }
 
-  private Date iat() {
-    return decoded().getBody().getIssuedAt();
+  private Instant iat() {
+    return checkNotNull(decoded().getBody().getIssuedAt(), "iat").toInstant();
   }
 
-  private Date exp() {
-    return decoded().getBody().getExpiration();
-  }
-
-  @Value.Check
-  protected void check() {
-    checkClaimIsPresent(iat(), IAT_PATH);
-    checkClaimIsPresent(exp(), IAT_PATH);
-    checkIatIsBeforeExp();
-  }
-
-  private static void checkClaimIsPresent(Object claim, String path) {
-    checkNotNull(claim, "'" + path + "' is required");
-  }
-
-  private void checkIatIsBeforeExp() {
-    checkState(iat().before(exp()), "'" + IAT_PATH + "' must be before '" + EXP_PATH + "'");
+  private Instant exp() {
+    return checkNotNull(decoded().getBody().getExpiration(), "exp").toInstant();
   }
 }
