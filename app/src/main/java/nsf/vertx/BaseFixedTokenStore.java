d5a1b2529d2d4e17759609281a4268f504449739
rtatton <rdt17@proton.me>
2023-03-20 10:37:53 -0500

    refactor: remove unused static factory methods

--- app/src/main/java/nsf/vertx/BaseFixedTokenStore.java
+++ app/src/main/java/nsf/vertx/BaseFixedTokenStore.java
@@ -1,11 +1,7 @@
 package nsf.vertx;
 
-import io.jsonwebtoken.Claims;
-import io.jsonwebtoken.Jws;
-import io.jsonwebtoken.Jwts;
 import io.vertx.core.Future;
 import java.security.PublicKey;
-import java.util.Map;
 import org.immutables.value.Value;
 
 @Value.Immutable
@@ -15,22 +11,6 @@ abstract class BaseFixedTokenStore extends AbstractTokenStore {
 
   public static TokenStore of(String encoded, TokenVerifier verifier) {
     return FixedTokenStore.builder().encoded(encoded).verifier(verifier).build();
-  }
-
-  public static TokenStore of(Jws<Claims> decoded, TokenVerifier verifier) {
-    return of(encode(decoded), verifier);
-  }
-
-  public static TokenStore of(Token token, TokenVerifier verifier) {
-    return of(token.encoded(), verifier);
-  }
-
-  @SuppressWarnings("unchecked")
-  private static String encode(Jws<Claims> decoded) {
-    return Jwts.builder()
-        .setHeader((Map<String, Object>) decoded.getHeader())
-        .setClaims(decoded.getBody())
-        .compact();
   }
 
   @Override
