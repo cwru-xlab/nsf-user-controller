21556926a99427ee247321347bad832cada6db89
rtatton <rdt17@proton.me>
2023-03-21 08:43:07 -0400

    feat: add negative assertion

--- app/src/test/java/nsf/vertx/auth/key/FixedPublicKeyStoreTests.java
+++ app/src/test/java/nsf/vertx/auth/key/FixedPublicKeyStoreTests.java
@@ -1,6 +1,7 @@
 package nsf.vertx.auth.key;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotEquals;
 
 import java.security.PublicKey;
 import org.junit.jupiter.api.Test;
@@ -8,10 +9,14 @@ import vertx.auth.KeyPairs;
 
 public class FixedPublicKeyStoreTests {
 
+  private static final PublicKey CORRECT = KeyPairs.correctPublic();
+  private static final PublicKey INCORRECT = KeyPairs.incorrectPublic();
+
   @Test
   public void whenPublicKeyIsRetrievedThenFutureEqualsConstructorValue() {
-    PublicKey publicKey = KeyPairs.correctPublic();
-    PublicKeyStore keyStore = FixedPublicKeyStore.of(publicKey);
-    assertEquals(publicKey, keyStore.get().result());
+    PublicKeyStore keyStore = FixedPublicKeyStore.of(CORRECT);
+    PublicKey keyStoreValue = keyStore.get().result();
+    assertEquals(CORRECT, keyStoreValue);
+    assertNotEquals(INCORRECT, keyStoreValue);
   }
 }
